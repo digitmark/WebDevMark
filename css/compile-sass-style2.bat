eeac3d6c34a12778784a6d154d7edbc5b7915e04
snawasth <sneha.awasthi@capgemini.com>
2018-12-27 10:44:57 +0530

    Style2.scss created

---
+++ css/compile-sass-style2.bat
@@ -0,0 +1,7 @@
+@echo off
+REM set /p ip="Enter the input file: "\r\n
+REM set /p op="Enter the output file: "\r\n
+REM sass --watch app/sass:public/stylesheets - For entire directory
+REM class sass --watch input.scss output.css
+timeout 1
+call sass --watch style2.scss style2.css
