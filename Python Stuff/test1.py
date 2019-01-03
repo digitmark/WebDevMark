773610b9d7cc9f47a13e96d28646f160c8c201f4
snawasth <sneha.awasthi@capgemini.com>
2019-01-03 18:05:57 +0530

    Python stuff.

---
+++ Python Stuff/test1.py
@@ -0,0 +1,8 @@
+#input("Yo")
+song = "Say Amen. It's Saturday night"
+print ("Song is: " +song)
+modsong = song.replace("Saturday", "Sunday")
+print("Mod song is: " +modsong)
+# i = "My name is"
+# length = len(i)
+# print (length)
