41f480e031b865bc81c39019ab24a42c701af0ae
rohkushw <rohit.kushwaha@capgemini.com>
2019-01-24 17:13:07 +0530

    Rohit Backup

---
+++ Rohit/Take data/Python Prac/ifelifelse.py
@@ -0,0 +1,13 @@
+x = int(input("Enter number - "))
+
+if x > 5:
+    print("x is greater than 5")
+
+elif x < 5:
+    print("x is smaller than 5")
+
+elif x == 5:
+    print("x is equal to 5")
+
+print("Bye Bye")
+
---
+++ Rohit/Take data/gitpush.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd d/ROHIT/my-app
+git checkout dev
+git add .
+git commit -am "made changes"
+git push
+echo Press Enter...
+read
